8097250a88025b90d6163f6488d0e2f342c61160
SirEdvin <me@siredvin.site>
2026-03-23 10:23:43 +0200

    Drop steam config since it is not working

--- src/main/java/site/siredvin/gttruesteam/config/TrueSteamConfig.java
+++ src/main/java/site/siredvin/gttruesteam/config/TrueSteamConfig.java
@@ -13,7 +13,6 @@ public class TrueSteamConfig {
     }
 
     private static void setupConfig(ForgeConfigSpec.Builder builder) {
-        SteamsConfig.setupConfig(builder);
         InfernalBoilerConfig.setupConfig(builder);
         GasPressurizerConfig.setupConfig(builder);
     }
