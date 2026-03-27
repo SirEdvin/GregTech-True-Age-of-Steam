03a5ba8000e79c8676386d73b7806db07ace71c5
SirEdvin <siredvin.dark@gmail.com>
2026-03-27 16:09:29 +0200

    basic spawner extraction machine

--- src/main/java/site/siredvin/gttruesteam/machines/spawner_extraction/SpawnerExtractionMachine.java
+++ src/main/java/site/siredvin/gttruesteam/machines/spawner_extraction/SpawnerExtractionMachine.java
@@ -7,33 +7,50 @@ import com.gregtechceu.gtceu.api.machine.multiblock.PartAbility;
 import com.gregtechceu.gtceu.api.pattern.FactoryBlockPattern;
 import com.gregtechceu.gtceu.api.pattern.Predicates;
 
+import com.gregtechceu.gtceu.common.data.GTBlocks;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.level.block.Blocks;
 
 import site.siredvin.gttruesteam.*;
 
 public class SpawnerExtractionMachine {
 
-    // 5 wide (X) x 7 tall (Y) x 5 deep (Z) hollow box, all sides ExtractionInfusedCasing
     public static MultiblockMachineDefinition MACHINE = GTTrueSteam.REGISTRATE
             .multiblock("spawner_extraction_machine", SpawnerExtractionMachineMachine::new)
             .rotationState(RotationState.NON_Y_AXIS)
             .recipeType(TrueSteamRecipeTypes.SPAWNER_EXTRACTION)
             .appearanceBlock(() -> TrueSteamBlocks.ExtractionInfusedCasing.get())
             .pattern(definition -> FactoryBlockPattern.start()
-                    .aisle("EEMEE", "EEEEE", "EEEEE", "EEEEE", "EEEEE", "EEEEE", "EEKEE")
-                    .aisle("EEEEE", "E   E", "E   E", "E   E", "E   E", "E   E", "EEEEE")
-                    .aisle("EEEEE", "E   E", "E   E", "E   E", "E   E", "E   E", "EEEEE")
-                    .aisle("EEEEE", "E   E", "E   E", "E   E", "E   E", "E   E", "EEEEE")
-                    .aisle("EEEEE", "EEEEE", "EEEEE", "EEEEE", "EEEEE", "EEEEE", "EEEEE")
+                    .aisle("EXMXE", "EGGGE", "EGGGE", "EGGGE", "EGGGE", "EGGGE", "EXXXE")
+                    .aisle("XEEEX", "EP PE", "EP PE", "EP PE", "EP PE", "EP PE", "XEEEX")
+                    .aisle("XEEEX", "EL LE", "EL LE", "EPSPE", "EL LE", "EL LE", "XEEEX")
+                    .aisle("XEEEX", "EP PE", "EP PE", "EP PE", "EP PE", "EP PE", "XEEEX")
+                    .aisle("EXKXE", "EGGGE", "EGGGE", "EGGGE", "EGGGE", "EGGGE", "EXXXE")
                     .where("E", Predicates.blocks(TrueSteamBlocks.ExtractionInfusedCasing.get()))
+                    .where("G", Predicates.blocks(GTBlocks.CASING_LAMINATED_GLASS.get()))
                     .where(" ", Predicates.blocks(Blocks.AIR))
+                    .where("L", Predicates.anyLamp().or(Predicates.blocks(Blocks.GLOWSTONE)))
+                    .where("P", Predicates.blocks(TrueSteamBlocks.ExtractionInfusedPipeCasing.get()))
                     .where("K", Predicates.controller(Predicates.blocks(definition.get())))
                     .where("M", Predicates.ability(PartAbility.MAINTENANCE))
+                    .where("S", Predicates.blocks(Blocks.SPAWNER))
+                    .where("X", Predicates.ability(PartAbility.IMPORT_FLUIDS).setPreviewCount(1)
+                            .or(Predicates.ability(PartAbility.IMPORT_ITEMS).setPreviewCount(1))
+                            .or(Predicates.ability(PartAbility.EXPORT_ITEMS).setPreviewCount(1))
+                            .or(Predicates.ability(PartAbility.EXPORT_FLUIDS).setPreviewCount(1))
+                            .or(Predicates.ability(PartAbility.INPUT_ENERGY).setMaxGlobalLimited(2)).or(Predicates.blocks(TrueSteamBlocks.ExtractionInfusedCasing.get())))
                     .build())
             .tooltips(TrueSteamLang.SEM_TOOLTIP_1)
+            .additionalDisplay(((iMultiController, components) -> {
+                if (iMultiController instanceof SpawnerExtractionMachineMachine spawnerExtractionMachineMachine) {
+                    var mobInside = spawnerExtractionMachineMachine.getMobInside();
+                    if (mobInside != null)
+                        components.add(Component.literal("Entity: " + mobInside.get().getDescription().getString()));
+                }
+            }))
             .workableCasingModel(
                     GTTrueSteam.id("block/extraction_infused_casing"),
-                    GTCEu.id("block/multiblock/electric_blast_furnace"))
+                    GTCEu.id("block/multiblock/gcym/large_extractor"))
             .register();
 
     public static void sayHi() {}
--- src/main/java/site/siredvin/gttruesteam/machines/spawner_extraction/SpawnerExtractionMachineMachine.java
+++ src/main/java/site/siredvin/gttruesteam/machines/spawner_extraction/SpawnerExtractionMachineMachine.java
@@ -1,11 +1,57 @@
 package site.siredvin.gttruesteam.machines.spawner_extraction;
 
 import com.gregtechceu.gtceu.api.machine.IMachineBlockEntity;
+import com.gregtechceu.gtceu.api.machine.multiblock.WorkableElectricMultiblockMachine;
 import com.gregtechceu.gtceu.api.machine.multiblock.WorkableMultiblockMachine;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Holder;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.level.block.entity.SpawnerBlockEntity;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.jetbrains.annotations.Nullable;
 
-public class SpawnerExtractionMachineMachine extends WorkableMultiblockMachine {
+import java.util.Objects;
+
+public class SpawnerExtractionMachineMachine extends WorkableElectricMultiblockMachine {
+
+    private @Nullable Holder<EntityType<?>> mobInside = null;
 
     public SpawnerExtractionMachineMachine(IMachineBlockEntity holder) {
         super(holder);
+    }
+
+    public BlockPos getSpawnerPos() {
+        var facing = getFrontFacing().getOpposite();
+        var currentPos = getPos();
+        return currentPos.relative(facing, 2).above(3);
+    }
+
+    protected void refreshMobInside() {
+        var level = getLevel();
+        if (level == null)
+            return;
+        var spawnerPos = getSpawnerPos();
+        var spawnerEntity = level.getBlockEntity(spawnerPos);
+        if (!(spawnerEntity instanceof SpawnerBlockEntity))
+            return;
+        var data = spawnerEntity.serializeNBT();
+        var entityTypeId = data.getCompound("SpawnData").getCompound("entity").getString("id");
+        var entityTypeHolder = ForgeRegistries.ENTITY_TYPES.getHolder(ResourceLocation.parse(entityTypeId));
+        entityTypeHolder.ifPresent(typeHolder -> mobInside = typeHolder);
+    }
+
+    @Override
+    public void onStructureFormed() {
+        super.onStructureFormed();
+    }
+
+    public @Nullable Holder<EntityType<?>> getMobInside() {
+        if (mobInside == null) {
+            refreshMobInside();
+        }
+        return mobInside;
     }
 }
