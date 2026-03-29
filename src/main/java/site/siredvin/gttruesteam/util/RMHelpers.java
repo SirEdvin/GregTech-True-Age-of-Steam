f5ced3482f7c93c5e5782991b672fb0366c4222c
SirEdvin <siredvin.dark@gmail.com>
2026-03-29 17:38:30 +0300

    Correct how RCC working with perfect OC

---
+++ src/main/java/site/siredvin/gttruesteam/util/RMHelpers.java
@@ -0,0 +1,16 @@
+package site.siredvin.gttruesteam.util;
+
+import com.gregtechceu.gtceu.api.GTValues;
+import com.gregtechceu.gtceu.api.recipe.GTRecipe;
+
+public class RMHelpers {
+    public static int calculateOC(GTRecipe recipe) {
+        var euT = recipe.getInputEUt().voltage();
+        for (int i = GTValues.V.length - 1; i > -1; i--) {
+            if (GTValues.V[i] < euT) {
+                return i;
+            }
+        }
+        return 0;
+    }
+}
